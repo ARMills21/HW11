be64b9ca4a8b67f9a73bf412196dd474cf5ba757
Alex Mills <alex@kcbas.com>
2020-05-07 21:18:22 -0500

    updated all my files

---
+++ server.js
@@ -0,0 +1,96 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const shortid = require("shortid");
+
+let newNoteArray = require('./db/db.json');
+
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    
+});
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+  
+  
+  
+app.post("/api/notes", (req, res) => {
+    
+    let noteId = shortid.generate();
+
+    let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text
+    };
+          
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+  
+        const allNotes = JSON.parse(data);
+  
+        allNotes.push(newNote);
+  
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+          if (err) throw err;
+          res.end();
+          console.log("Note created!")
+        });
+      });
+    });
+ 
+
+app.delete("/api/notes/:id", (req, res) => {
+    let noteId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        
+    const allNotes = JSON.parse(data);
+    const newAllNotes = allNotes.filter(note => note.id != noteId);
+
+    fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+        if (err) throw err;
+        res.end();
+        console.log("Note deleted!")
+      });
+    });
+  });
+
+app.use('*', (_, res) => {
+	res.redirect('/');
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+© 2020 GitHub, Inc.
